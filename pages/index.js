afb2516cab909c6f9a78514385b7659304944be9
rizkyRosyidHidayat <rizky.hidayat@students.amikom.ac.id>
2021-03-08 12:52:22 +0700

    add banner section

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,28 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>Sedekah Air Wonosobo</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <section className={styles.banner}>
+        <div className="text-center text-white w-2/3 mx-auto relative z-10 -mb-48">
+          <h1 className="text-6xl font-bold">Sedekah Air Wonosobo</h1>
+          <p className="my-8">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Cum quos dolorem voluptates et minus culpa ducimus corrupti, 
+            dignissimos amet, tempore sequi doloremque vel nisi. Aliquid 
+            similique fuga incidunt corrupti exercitationem!
+          </p>
+          <button className="button mb-12">
+            Lihat Selengkapnya
+          </button>
+          <img src="/image/photo_banner.png" alt="photo banner" width="100%" height="auto" />
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      </section>
+    </>
   )
 }
