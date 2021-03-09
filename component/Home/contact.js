f6412d99e52b727407b60cd523d87e7fcb00c7f0
rizkyRosyidHidayat <rizky.hidayat@students.amikom.ac.id>
2021-03-09 22:40:31 +0700

    add contact section

---
+++ component/Home/contact.js
@@ -0,0 +1,53 @@
+function Contact() {
+  return(
+    <section className="py-20">
+      <div className="container">
+        <div className="grid grid-cols-3 gap-8">
+          <div>
+            <div className="flex mb-4">
+              <div className="w-12 h-12 flex-shrink-0 rounded-full flex items-center justify-center bg-gray-200 text-white font-bold mr-4">
+                s
+              </div>
+              <div>
+                <h4 className="mb-2 font-bold text-lg">Judul</h4>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                  Cum quos dolorem
+                </p>
+              </div>
+            </div>
+            <div className="flex mb-4">
+              <div className="w-12 h-12 flex-shrink-0 rounded-full flex items-center justify-center bg-gray-200 text-white font-bold mr-4">
+                s
+              </div>
+              <div>
+                <h4 className="mb-2 font-bold text-lg">Judul</h4>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                  Cum quos dolorem
+                </p>
+              </div>
+            </div>
+            <div className="flex mb-4">
+              <div className="w-12 h-12 flex-shrink-0 rounded-full flex items-center justify-center bg-gray-200 text-white font-bold mr-4">
+                s
+              </div>
+              <div>
+                <h4 className="mb-2 font-bold text-lg">Judul</h4>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                  Cum quos dolorem
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-2">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3956.9563928608527!2d109.90090411443015!3d-7.358785174448189!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7aa0ff266df64d%3A0xbb86598aeb9508b!2sAlun-Alun%20Wonosobo!5e0!3m2!1sen!2sid!4v1615301204474!5m2!1sen!2sid" width="100%" height="450" style={{ border: '0' }} allowfullscreen="" loading="lazy"></iframe>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Contact
