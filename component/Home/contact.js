7caf93372396b602372cd3d818cc8c4fd314fe78
rizkyRosyidHidayat <rizky.hidayat@students.amikom.ac.id>
2021-03-10 15:46:47 +0700

    edit attr

--- component/Home/contact.js
+++ component/Home/contact.js
@@ -42,7 +42,7 @@ function Contact() {
             </div>
           </div>
           <div className="col-span-2">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3956.9563928608527!2d109.90090411443015!3d-7.358785174448189!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7aa0ff266df64d%3A0xbb86598aeb9508b!2sAlun-Alun%20Wonosobo!5e0!3m2!1sen!2sid!4v1615301204474!5m2!1sen!2sid" width="100%" height="450" style={{ border: '0' }} allowfullscreen="" loading="lazy"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3956.9563928608527!2d109.90090411443015!3d-7.358785174448189!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7aa0ff266df64d%3A0xbb86598aeb9508b!2sAlun-Alun%20Wonosobo!5e0!3m2!1sen!2sid!4v1615301204474!5m2!1sen!2sid" width="100%" height="450" style={{ border: '0' }} allowFullScreen="" loading="lazy"></iframe>
           </div>
         </div>
       </div>
