17c8e3d2c50d2bfbfa122ffe292028107a791482
rizkyRosyidHidayat <rizky.hidayat@students.amikom.ac.id>
2021-03-11 10:12:24 +0700

    make style contact section

--- component/Home/contact.js
+++ component/Home/contact.js
@@ -1,39 +1,17 @@
+import style from '../../styles/Contact.module.css'
+
 function Contact() {
   return(
-    <section className="py-20 bg-white">
-      <div className="container">
-        <div className="grid grid-cols-3 gap-8">
-          <div className="lg:col-span-1 col-span-3">
-            <div className="flex mb-4">
-              <div className="w-12 h-12 flex-shrink-0 rounded-full flex items-center justify-center bg-gray-200 text-white font-bold mr-4">
+    <section className={style.contact}>
+      <div className="containerGlobal">
+        <div className={style.row}>
+          <div className={style.detail}>
+            <div className={style.item}>
+              <div className={style.avatar}>
                 s
               </div>
               <div>
-                <h4 className="mb-2 font-bold text-lg">Judul</h4>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                  Cum quos dolorem
-                </p>
-              </div>
-            </div>
-            <div className="flex mb-4">
-              <div className="w-12 h-12 flex-shrink-0 rounded-full flex items-center justify-center bg-gray-200 text-white font-bold mr-4">
-                s
-              </div>
-              <div>
-                <h4 className="mb-2 font-bold text-lg">Judul</h4>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                  Cum quos dolorem
-                </p>
-              </div>
-            </div>
-            <div className="flex mb-4">
-              <div className="w-12 h-12 flex-shrink-0 rounded-full flex items-center justify-center bg-gray-200 text-white font-bold mr-4">
-                s
-              </div>
-              <div>
-                <h4 className="mb-2 font-bold text-lg">Judul</h4>
+                <h4 className={style.title}>Judul</h4>
                 <p>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                   Cum quos dolorem
@@ -41,7 +19,7 @@ function Contact() {
               </div>
             </div>
           </div>
-          <div className="lg:col-span-2 col-span-3">
+          <div className={style.map}>
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3956.9563928608527!2d109.90090411443015!3d-7.358785174448189!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7aa0ff266df64d%3A0xbb86598aeb9508b!2sAlun-Alun%20Wonosobo!5e0!3m2!1sen!2sid!4v1615301204474!5m2!1sen!2sid" width="100%" height="450" style={{ border: '0' }} allowFullScreen="" loading="lazy"></iframe>
           </div>
         </div>
