600ee333e3756c5a23a012fca663502636a44752
rizkyRosyidHidayat <rizky.hidayat@students.amikom.ac.id>
2021-03-11 09:34:27 +0700

    make style partner

--- component/partner.js
+++ component/partner.js
@@ -1,25 +1,27 @@
+import styles from '../styles/Partner.module.css'
+
 function Partner() {
   return (
-    <section className="bg-white py-20">
-      <div className="container">
-        <h2 className="font-bold text-xl text-center mb-16">Pihak yang Mendukung</h2>
-        <div className="flex flex-wrap justify-center items-center">
-          <div className="md:w-1/6 w-1/3 px-4 md:mb-0 mb-8">
+    <section className={styles.partner}>
+      <div className="containerGlobal">
+        <h2 className={styles.title}>Pihak yang Mendukung</h2>
+        <div className={styles.row}>
+          <div className={styles.col}>
             <img src="/image/adidas.png" alt="logo" width="120px"/>
           </div>
-          <div className="md:w-1/6 w-1/3 px-4 md:mb-0 mb-8">
+          <div className={styles.col}>
             <img src="/image/nike.png" alt="logo" width="120px"/>
           </div>
-          <div className="md:w-1/6 w-1/3 px-4 md:mb-0 mb-8">
+          <div className={styles.col}>
             <img src="/image/adidas.png" alt="logo" width="120px"/>
           </div>
-          <div className="md:w-1/6 w-1/3 px-4 md:mb-0 mb-8">
+          <div className={styles.col}>
             <img src="/image/nike.png" alt="logo" width="120px"/>
           </div>
-          <div className="md:w-1/6 w-1/3 px-4 md:mb-0 mb-8">
+          <div className={styles.col}>
             <img src="/image/adidas.png" alt="logo" width="120px"/>
           </div>
-          <div className="md:w-1/6 w-1/3 px-4 md:mb-0 mb-8">
+          <div className={styles.col}>
             <img src="/image/nike.png" alt="logo" width="120px"/>
           </div>
         </div>
