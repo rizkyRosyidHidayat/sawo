dd3541fb08647866aa2db9b1e9ad2608a2f99224
rizkyRosyidHidayat <rizky.hidayat@students.amikom.ac.id>
2021-03-11 10:03:01 +0700

    make style donation section

--- component/Home/donation.js
+++ component/Home/donation.js
@@ -5,28 +5,23 @@ function Donation() {
     { name: 'Hamba Allah', value: 500000 }
   ]
   return (
-    <div className="relative">
+    <div className="relativeGlobal">
       <div className={styles.bgDonation}></div>
-      <section className={`${styles.banner} text-white h-screen flex items-center`}>
+      <section className={`${styles.banner}`}>
         <div className="container">
-          <h3 className="text-center font-bold text-3xl">
+          <h3 className="title-section">
             Total Donasi
           </h3>
           <div className="line-title-section-white"></div>
-          <div className="text-center mb-6">
-            <h1 className="lg:text-8xl md:text-7xl sm:text-6xl text-5xl font-bold my-12">Rp. 100.000.000 ,00</h1>
-            <div className="font-bold">DONATUR TERBARU</div>
+          <div className="textCenter mb6">
+            <h1 className={styles.donation}>Rp. 100.000.000 ,00</h1>
+            <b>DONATUR TERBARU</b>
           </div>
-          <div className="lg:w-1/2 md:w-full mx-auto">
-            <div className="flex items-center p-3 rounded text-gray-800 mb-3" style={{ backgroundColor: 'rgba(255, 255, 255, 0.4)' }}>
-              <div className="w-12 h-12 rounded-full flex items-center justify-center bg-green-600 text-white font-bold mr-4">S</div>
-              <span className="mr-auto">ss</span>
-              <div className="font-bold">Rp. 500.000</div>
-            </div>
-            <div className="flex items-center p-3 rounded text-gray-800" style={{ backgroundColor: 'rgba(255, 255, 255, 0.4)' }}>
-              <div className="w-12 h-12 rounded-full flex items-center justify-center bg-green-600 text-white font-bold mr-4">S</div>
-              <span className="mr-auto">ss</span>
-              <div className="font-bold">Rp. 500.000</div>
+          <div className={styles.row}>
+            <div className={styles.col} style={{ backgroundColor: 'rgba(255, 255, 255, 0.4)' }}>
+              <div className={styles.avatar}>S</div>
+              <span className="mrAuto">ss</span>
+              <b>Rp. 500.000</b>
             </div>
           </div>
         </div>
