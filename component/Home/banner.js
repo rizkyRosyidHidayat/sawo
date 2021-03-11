bdfa4c038cbe815c3df7eb6677c499497619dd65
rizkyRosyidHidayat <rizky.hidayat@students.amikom.ac.id>
2021-03-11 09:03:00 +0700

    make class name banner section

--- component/Home/banner.js
+++ component/Home/banner.js
@@ -5,15 +5,15 @@ function Banner() {
     <>
       <div className={styles.bgBanner}></div>
       <section className={styles.banner}>
-        <div className={styles.descBanner}>
-          <h1 className="sm:text-6xl text-5xl font-bold">Sedekah Air Wonosobo</h1>
-          <p className="my-8">
+        <div className={styles.desc}>
+          <h1 className={styles.title}>Sedekah Air Wonosobo</h1>
+          <p className="my8">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. 
             Cum quos dolorem voluptates et minus culpa ducimus corrupti, 
             dignissimos amet, tempore sequi doloremque vel nisi. Aliquid 
             similique fuga incidunt corrupti exercitationem!
           </p>
-          <button className="button mb-12">
+          <button className="button mb12">
             Lihat Selengkapnya
           </button>
           <img src="/image/photo_banner.png" alt="photo banner" width="100%" height="auto" />
