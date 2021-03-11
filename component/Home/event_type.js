3bf7db3b28e41a7232221a4d1afbf5498b2d5845
rizkyRosyidHidayat <rizky.hidayat@students.amikom.ac.id>
2021-03-11 09:18:30 +0700

    make style event type section

--- component/Home/event_type.js
+++ component/Home/event_type.js
@@ -1,40 +1,24 @@
+import styles from '../../styles/Event.module.css'
+
 function EventType() {
   return (
-    <section style={{ backgroundColor: '#F5F7FC' }} className="pt-64 pb-24">
-      <div className="container">
-        <div className="shadow-2xl rounded-lg p-8 bg-white">
-          <h3 className="text-center font-bold text-3xl">
+    <section style={{ backgroundColor: '#F5F7FC' }} className={styles.eventType}>
+      <div className="containerGlobal">
+        <div className={styles.card}>
+          <h3 className="text-section">
             Jenis Kegiatan
           </h3>
           <div className="line-title-section"></div>
-          <p className="mt-8 text-center">
+          <p className={styles.desc}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. 
             Cum quos dolorem voluptates et minus culpa ducimus corrupti, 
             dignissimos amet, tempore sequi doloremque vel nisi. Aliquid 
             similique fuga incidunt corrupti exercitationem!
           </p>
-          <div className="grid md:grid-cols-3 sm:grid-cols-2 gap-8 mt-32">
-            <div className="text-center">
-              <div className="rounded-full w-24 h-24 bg-blue-200 mx-auto"></div>
-              <h4 className="font-bold text-xl my-6">Nama Tipe Kegiatan</h4>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Cum quos dolorem voluptates et minus culpa ducimus corrupti, 
-                dignissimos amet.
-              </p>
-            </div>
-            <div className="text-center">
-              <div className="rounded-full w-24 h-24 bg-blue-200 mx-auto"></div>
-              <h4 className="font-bold text-xl my-6">Nama Tipe Kegiatan</h4>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Cum quos dolorem voluptates et minus culpa ducimus corrupti, 
-                dignissimos amet.
-              </p>
-            </div>
-            <div className="text-center">
-              <div className="rounded-full w-24 h-24 bg-blue-200 mx-auto"></div>
-              <h4 className="font-bold text-xl my-6">Nama Tipe Kegiatan</h4>
+          <div className={styles.row}>
+            <div className="textCenter">
+              <div className={styles.avatar}></div>
+              <h4 className={styles.name}>Nama Tipe Kegiatan</h4>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                 Cum quos dolorem voluptates et minus culpa ducimus corrupti, 
