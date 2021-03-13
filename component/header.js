c3b931b6b3dbeffef2f3c371d8b749e26abf86e0
rizkyRosyidHidayat <rizky.hidayat@students.amikom.ac.id>
2021-03-13 07:32:56 +0700

    add header section

---
+++ component/header.js
@@ -0,0 +1,28 @@
+import style from '../styles/Header.module.css'
+import cn from 'classnames'
+
+function Header(props) {
+  return (
+    <header className={props.active?style.header+' '+style.active:style.header}>
+      <div className="containerGlobal">
+        <div className={props.active?style.contentHeader+' '+style.active:style.contentHeader}>
+          <a href="#" className={style.brand}>
+            <img src="/vercel_white.svg" alt="logo brand" style={{ height: props.active?'30px':'40px', transition: 'all 0.5s ease' }} />
+          </a>
+          <div className={style.menu}>
+            <a href="#" className="text-blue-500">Beranda</a>
+            <a href="#">Kegiatan</a>
+            <a href="#">Galeri</a>
+            <a href="#">Donasi</a>
+            <a href="#">Kontak</a>
+            {/* <a href="javascript:void(0)">
+              <img src="/icon/menu.svg" alt="icon menu"/>
+            </a> */}
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,34 @@
+import { useEffect, useState } from "react";
 import Head from 'next/head'
 import Footer from '../component/footer'
+import Header from '../component/header'
 import Banner from '../component/Home/banner'
 import Contact from '../component/Home/contact'
 import Donation from '../component/Home/donation'
 import EventType from '../component/Home/event_type'
 
 export default function Home() {
+  const [active, setActive] = useState(false)
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  })
+  
+  const handleScroll = () => {
+    if (window.scrollY > 50) {
+      setActive(true)
+    } else {
+      setActive(false)
+    }
+  }
   return (
     <>
       <Head>
         <title>Sedekah Air Wonosobo</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Header active={active}></Header>
 
       <Banner></Banner>
 
