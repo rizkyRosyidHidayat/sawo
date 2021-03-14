cfe3555f728f31e752af659f5c98ab2854422679
rizkyRosyidHidayat <rizky.hidayat@students.amikom.ac.id>
2021-03-14 11:02:19 +0700

    add hidden menu in header

--- component/header.js
+++ component/header.js
@@ -1,7 +1,10 @@
 import style from '../styles/Header.module.css'
-import cn from 'classnames'
+import { useState } from "react";
 
 function Header(props) {
+  const [visible, setVisible] = useState(false)
+
+  const handleClick = () => setVisible(!visible)
   return (
     <header className={props.active?style.header+' '+style.active:style.header}>
       <div className="containerGlobal">
@@ -9,16 +12,23 @@ function Header(props) {
           <a href="#" className={style.brand}>
             <img src="/vercel_white.svg" alt="logo brand" style={{ height: props.active?'30px':'40px', transition: 'all 0.5s ease' }} />
           </a>
-          <div className={style.menu}>
+          <div className={style.menu+' '+style.showOnlyLarge}>
             <a href="#">Beranda</a>
             <a href="#">Kegiatan</a>
             <a href="#">Galeri</a>
             <a href="#">Donasi</a>
             <a href="#">Kontak</a>
-            {/* <a href="javascript:void(0)">
-              <img src="/icon/menu.svg" alt="icon menu"/>
-            </a> */}
           </div>
+          <div className={style.menu+' '+style.hiddenOnlyLarge}>
+              <img src="/icon/menu.svg" alt="icon menu" className={style.btnMenu} onClick={handleClick} />
+          </div>
+        </div>
+        <div className={visible?style.hiddenMenu+' '+style.active:style.hiddenMenu}>
+          <a href="#">Beranda</a>
+          <a href="#">Kegiatan</a>
+          <a href="#">Galeri</a>
+          <a href="#">Donasi</a>
+          <a href="#">Kontak</a>
         </div>
       </div>
     </header>
